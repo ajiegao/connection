125b0b44553eaf0e054c170d8e9cac9693f460c4
jie gao <ajiegaoxf@gmail.com>
2014-01-11 01:44:04 +0800

    update

--- flip.js
+++ flip.js
@@ -18,10 +18,19 @@ var Boot = {
         
     },
     create : function(){
-        this.select = game.add.group()
+        this.dia = 100 
         this.lastselect = null
         this.move = false
+		this.select = game.add.group()
         this.Groupeobjs = game.add.group()
+		
+		//this.delPoints = []
+        
+        this.Groupeobjs.y = 222
+        this.select.y = 222
+        
+		//this.dels = game.add.group()
+		//this.animas = game.add.group()
         //fip = game.add.sprite(0, 0, "circle")
         //fip2 = new Phaser.Sprite(game, 88, 88, "circle")
         //Groupeobjs.add(fip2)
@@ -32,22 +41,13 @@ var Boot = {
         for(var i=0; i<6; i++){
         
             for(var j=0; j<6; j++){
-                var r = Math.floor(Math.random()*5+1)
-                var face = 'w'+r
-                var flips = new Phaser.Sprite(game, i*100+30, j*100, face)
-                flips.scale.setTo(0.2, 0.2)
-                flips.inputEnabled = true;
-                flips.input.pixelPerfect = true;
-                flips.input.useHandCursor = true;
-                flips.alpha = .6
-                flips.r = i
-                flips.c = j
-                flips.t = face
+                var flips  = this.addnewobj(i*this.dia+30, j*this.dia)
+                flips.r = j
+                flips.c = i
                 flips.n = 'w'+i+'-'+j
                 this.Groupeobjs.add(flips)
                 
-                flips.events.onInputDown.add(this.funcDown, this)
-                flips.events.onInputOver.add(this.funcOver, this)
+                
                 
             }
         
@@ -57,23 +57,41 @@ var Boot = {
 		game.input.onUp.add(this.funcUp, this)
 
     },
+    
     render: function(){
+        
         
     },
     update: function(){
         
+        var that = this
+        //console.log(this.Groupeobjs)
+        
+        this.Groupeobjs.forEach(function(item){
+            
+            if(item.y < item.r*that.dia){
+                item.y += 5
+            }else{
+                item.y = item.r*that.dia
+            }
+            
+            
+        })
     },
     funcDown: function(f){
         this.move = true
-
+        
         f.alpha = 1
         this.select.add(f)
         this.lastselect = f
-    
+        console.log(f.t)
     },
     funcOver: function(f){
        
         if(!this.move) return false;
+        
+        //console.log(f.t +'|' +this.lastselect.t)
+        
         if(f.t !== this.lastselect.t) return false;
         //if(f.i == this.lastselect.i && f.j == this.lastselect.j) return ;
         
@@ -90,14 +108,114 @@ var Boot = {
         
     },
     funcUp: function(){
+		var that = this
         this.move = false
-        
+ 
         var isdel = true
         if(this.select.length > 2){
-            this.select.removeAll()
+		
+			this.updateobjs()
+            
+            
         }else{
             this.select.setAll('alpha', 0.6)
+			//console.log(this.select.length)
+			
+			while(this.select.length > 0){
+				var item = this.select.getFirstAlive()
+				that.Groupeobjs.add(item)
+			}
+			
         }
         
+        this.lastselect = null
+
+        
+    },
+    updateobjs: function(){
+    
+        var that = this
+        var pobj = this.select
+        var plength = this.select.length
+        
+        var hascol = -10
+        var addrow = -1
+        
+        
+        this.select.forEach(function(p){
+        
+            var newc = p.c
+            
+            if(hascol == newc){              
+                addrow--  
+            }else{
+                addrow = -1
+            }
+            hascol = newc
+            
+            var flips  = that.addnewobj(newc*that.dia+30, -1*that.dia)
+            flips.r = addrow
+            flips.c = p.c
+
+            that.Groupeobjs.add(flips)
+            
+        })
+        
+
+        that.select.sort('r')
+        
+        this.Groupeobjs.forEach(function(item){
+        
+            that.select.forEach(function(p){
+                var pc = p.c
+                var pr = p.r
+                
+                if(item.c == pc && item.r < pr){
+                
+                    item.r += 1
+                    
+                }
+            
+            })
+
+            //console.log(item.r + '--'+item.c)
+        })
+        
+        
+        this.select.removeAll()
+    
+    
+    },
+    addnewobj: function(x, y){
+        var r = Math.floor(Math.random()*5+1)
+        var face = 'w'+r
+        var flips = new Phaser.Sprite(game, x, y, face)
+        flips.scale.setTo(0.2, 0.2)
+        flips.inputEnabled = true;
+        flips.input.pixelPerfect = true;
+        flips.input.useHandCursor = true;
+        flips.alpha = .6
+
+        flips.t = face
+        
+        flips.events.onInputDown.add(this.funcDown, this)
+        flips.events.onInputOver.add(this.funcOver, this)
+                
+        return flips
     }
+//	,
+//	
+//	addanima: function(item){
+//	
+//		if(item.r > 0){
+//		
+//			for(var rr = 0; rr < item.r; rr++){
+//				this.dia
+//				item.c
+//			}
+//		
+//		
+//		}
+//
+//	}
 }
